f24e0f0aef2f657c69ba844b1841d677f39ef611
Neto Sena <neetosena@gmail.com>
2024-07-09 15:49:46 +0100

    Update useScrollTop.js

--- src/components/useScrollTop.js
+++ src/components/useScrollTop.js
@@ -1,13 +1,15 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useGlobalContext } from "./context"
 
 const useScrollTop = () => {
   const location = useLocation();
+  const { scrollToSection } = useGlobalContext()
 
   useEffect(() => {
     console.log('Pathname changed to:', location.pathname);
     window.scrollTo({top: 0, behavior: "smooth" })
-  }, [location])
+  }, [location, scrollToSection])
   
   
   
