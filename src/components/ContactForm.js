f088ba5908752a8794754fd6aaa4a2a95617f8e0
Neto Sena <neetosena@gmail.com>
2024-07-11 00:31:47 +0100

    contact bot

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -48,6 +48,7 @@ const ContactForm = () => {
           netlify-honeypot="bot-field"
         >
           <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="bot-field" />
           <div>
             <Field
               className="input-field"
