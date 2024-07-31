decf398cd8332b70a9d3dfb4967caf04e80b7016
Neto Sena <neetosena@gmail.com>
2024-07-31 10:21:01 +0100

    Update ContentfulClient.js
    Update the Initial for the .env to be "REACT_APP"

--- src/components/ContentfulClient.js
+++ src/components/ContentfulClient.js
@@ -1,8 +1,8 @@
 import { createClient } from "contentful";
 
 const client = createClient({
-  space: process.env.SPACE,
-  accessToken: process.env.ACCESS_TOKEN,
+  space: process.env.REACT_APP_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
 
 export default client;
