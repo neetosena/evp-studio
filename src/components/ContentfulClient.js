7f7a5e62014933598b993f0ad8aff84142eb26e5
Neto Sena <neetosena@gmail.com>
2026-04-10 14:06:44 +0100

    Add GraphQL fetch for new Contentful data

--- src/components/ContentfulClient.js
+++ src/components/ContentfulClient.js
@@ -3,6 +3,7 @@ import { createClient } from "contentful";
 const client = createClient({
   space: process.env.REACT_APP_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  environment: process.env.REACT_APP_ENVIRONMENT,
 });
 
 export default client;
